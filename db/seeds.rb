e83ca4c0eac1bcfca2e35e2a93cbc373aa7cf137
Fred Fooks <fredjfooks@gmail.com>
2018-05-21 17:03:39 +0200

    done next to nothing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Users.destroy_all
+Company.destroy_all
+
+puts 'Creating Users...'
+
+user = [{email: "fredfooks@me.com", password: 121212},
+        {email: "tina@gotto.do", password: 232323},
+        {email: "magnus@minimus.latin", password: 34343434}
+]
+
+
+puts 'Creating Legs...'
+gazelle_runners = [
+  { first_name: "Fred", last_name: "Fooks",
+    photo: "https://avatars1.githubusercontent.com/u/37072793?s=400&u=a3da37beff2056ff305a13654be9aa05ceca92af&v=4", description: "Masters in Engineering Science at Sommerville College, University of Oxford. I want to learn to code to have a great understand of the applications of technology. After Le wagon I am working as a technology business analyst for Capgemini London" },
+
+  { first_name: "Tina", last_name: "Turner",
+    photo: "https://avatars0.githubusercontent.com/u/38000034?s=400&v=4", description: "After working for a few years for the UN and the Municipality of Copenhagen, I decided that I wanted to try something completely different. I found out that I wanted to try to become a web developer." },
+
+ { first_name: "Magnus", last_name: "Parvus",
+   photo: "https://avatars1.githubusercontent.com/u/37999937?s=400&v=4", description: "I just finished High School and have always been interested in Coding. In my off year I decided to do the Le Wagon Bootcamp as I had seen it was the best/only one in Denmark :)" }
+]
+
+
+puts 'Creating Companies...'
+companies = [name: "Le wagon", name: "Copenhagen FC", name: "Ralph Lauren", name: "Vivino", name: "Carbnb"]
+
